1a7673b0abb19f551cb2ee45ac750d57a844ab09
Nathan Thomas <thomas.nathanm@gmail.com>
2022-09-02 22:38:52 -0500

    Add gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,16 @@
+// Activates the image gallery.
+// The main task is to attach an event listener to each image in the gallery
+// and respond appropriately on click.
 function activateGallery() {
-  alert("Hello from the gallery file!");
+  let thumnails = document.querySelectorAll("#gallery-thumbs > div > img");
+  let mainImage = document.querySelector("#gallery-photo > img");
+
+  thumnails.forEach(function(thumbnail) {
+    thumbnail.addEventListener("click", function() {
+      // Set clicked image as main image.
+      let newImageSrc = thumbnail.dataset.largeVersion;
+      mainImage.setAttribute("src", newImageSrc);
+      mainImage.setAttribute("alt", thumbnail.alt);
+    });
+  });
 }
